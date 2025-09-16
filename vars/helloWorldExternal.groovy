ab339a48ba100162c003ffc27ca3634028c8b744
Daniel Rego Calvo <dregocal@emeal.nttdata.com>
2025-09-16 12:39:42 +0200

    Corrected path again

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) { 
-    def scriptContents = libraryResource "/scripts/helloWorld.bat"
+    def scriptContents = libraryResource "scripts/helloWorld.bat"
     writeFile file: "helloWorld.bat", text: scriptContents
     bat "helloWorld.bat ${config.name} ${config.dayOfWeek}"
 }
